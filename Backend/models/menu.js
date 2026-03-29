5b2e238b51d16a2c47aa796bc5e8d5e88341b6d2
Moulya15 <ksmoulya2003@gmail.com>
2026-03-29 11:39:09 +0530

    menu schema

---
+++ Backend/models/menu.js
@@ -0,0 +1,29 @@
+//we need categories like staters main course
+//inside eac category: item
+//wjole mneu belong to restaurant
+
+
+
+
+const mongoose= require=("mongoose");
+const menuSchema= new mongoose.Schema({
+    menu:[{
+        category:{type:String, required:true},
+        items:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"FoodItem"// while exporting it should be in capital letter so here we have taken the captial F itself
+        }]
+    }],
+    restaurant:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:String
+    }
+},
+{
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
+}
+)
+
+Menu = mongoose.model("Menu",menuSchema);
+module.exports=Menu;
